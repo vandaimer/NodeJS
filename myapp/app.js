4c943d8773b4a1f18331cf5b429d594e0446fd40
Luiz Filipe <moresco.luiz@gmail.com>
2015-01-22 23:22:19 -0200

    Adicionado imagen com jade

--- myapp/app.js
+++ myapp/app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -22,8 +19,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/*--------------------------*/
+/*-AQUI VAI A PORRA TODA-*/
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
 app.use('/', routes);
 app.use('/users', users);
+/*-FIM DA PORRA TODO-*/
+/*--------------------------*/
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var data = { title:'Express Title', srcImg : 'imgs/nodejs.png' }
+	res.render('index', data);
 });
 
 module.exports = router;
